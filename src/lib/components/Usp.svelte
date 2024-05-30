5badb3629ffac05548488e53c03c8a9a920d57ca
Liam Lyness <liamlyness97@gmail.com>
2024-05-30 21:31:15 +0100

    Added the bubble section

---
+++ src/lib/components/Usp.svelte
@@ -0,0 +1,62 @@
+<div class="relative mt-5 w-full overflow-hidden rounded-lg bg-[#E7EEF7] py-32">
+	<div
+		class="absolute -right-[150px] -top-[270px] z-10 h-[650px] w-[650px] rounded-full bg-white opacity-40"
+	></div>
+	<div class="relative z-20 mx-auto max-w-7xl">
+		<p class="font-display line w-1/2 text-balance text-4xl font-semibold text-[#0C2D56]">
+			Changing the way we use the most abundant biopolymer on earth
+		</p>
+		<p class="font-body mt-3 text-lg">
+			Turning cellulose into functional, high performance ingredients
+		</p>
+		<div class="mt-16 grid grid-cols-3 gap-36">
+			<div class="flex flex-col justify-between gap-5">
+				<div class="flex flex-col gap-3">
+					<img class="w-12" src="/natural-colours.png" alt="Natural Colours" />
+					<p class="font-display text-3xl font-semibold text-[#0C2D56]">Natural Colours</p>
+					<p class="font-body font-medium text-black">
+						Our cellulose particles are highly functional, 100% natural plant-based and can be used
+						in a wide range of formulations.
+					</p>
+				</div>
+				<div>
+					<a
+						class="font-body justify-items-end self-start border-b border-black py-3 font-bold"
+						href="/">Find out more</a
+					>
+				</div>
+			</div>
+			<div class="flex flex-col justify-between gap-5">
+				<div class="flex flex-col gap-3">
+					<img class="w-12" src="/responsible.png" alt="Responsible" />
+					<p class="font-display text-3xl font-semibold text-[#0C2D56]">Responsible</p>
+					<p class="font-body font-medium text-black">
+						Our products are biodegradable, produced responsibly and do not cause harm to people,
+						society or the environment.
+					</p>
+				</div>
+				<div>
+					<a
+						class="font-body self-start justify-self-end border-b border-black py-3 align-bottom font-bold"
+						href="/">Find out more</a
+					>
+				</div>
+			</div>
+			<div class="flex flex-col justify-between gap-5">
+				<div class="flex flex-col gap-3">
+					<img class="w-12" src="/climate-action.png" alt="Climate Action" />
+					<p class="font-display text-3xl font-semibold text-[#0C2D56]">Climate Action</p>
+					<p class="font-body font-medium text-black">
+						Our production avoids carbon emissions compared to existing material processing such as
+						titanium dioxide production.
+					</p>
+				</div>
+				<div>
+					<a class="font-body self-end border-b border-black py-3 font-bold" href="/"
+						>Find out more</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,54 @@
 <script>
 	import Hero from '$lib/components/Hero.svelte';
+	import Usp from '$lib/components/Usp.svelte';
 </script>
 
 <Hero />
 
-<div class="mt-5 w-full rounded-lg bg-[#E7EEF7] py-28">
-	<div class="mx-auto max-w-7xl">
-		<p class="font-display line w-1/2 text-balance text-4xl font-semibold text-[#0C2D56]">
-			Changing the way we use the most abundant biopolymer on earth
-		</p>
-		<p class="font-body mt-3 text-lg">
-			Turning cellulose into functional, high performance ingredients
-		</p>
-		<div class="mt-16 grid grid-cols-3 gap-36">
-			<div class="flex flex-col justify-between gap-5">
-				<div class="flex flex-col gap-3">
-					<img class="w-12" src="/natural-colours.png" alt="Natural Colours" />
-					<p class="font-display text-3xl font-semibold text-[#0C2D56]">Natural Colours</p>
-					<p class="font-body font-medium text-black">
-						Our cellulose particles are highly functional, 100% natural plant-based and can be used
-						in a wide range of formulations.
+<Usp />
+
+<div class="w-full py-32">
+	<div class="relative mx-auto grid max-w-5xl grid-cols-4 grid-rows-3 gap-10">
+		<div class="col-span-2">
+			<p class="font-display mb-5 text-pretty text-3xl font-semibold tracking-wide text-[#7FA1F8]">
+				Play a role in our efforts to improve the health of our planet
+			</p>
+			<p class="font-body text-lg font-semibold">
+				Functional ingredients produced from renewable resources
+			</p>
+		</div>
+		<div class="relative col-span-2 row-span-3">
+			<div
+				class="relative z-30 flex aspect-square w-[85%] items-center justify-center rounded-full bg-[#0C2D56] text-white"
+			>
+				<div class="w-2/3">
+					<p class="font-display mb-5 text-2xl font-semibold">Thinking of<br />the future</p>
+					<p class="font-body text-sm leading-loose">
+						We envision a future where products are created from functional ingredients produced
+						from renewable resources using sustainable and environmentally responsible processes.
 					</p>
-				</div>
-				<div>
-					<a
-						class="font-body justify-items-end self-start border-b border-black py-3 font-bold"
-						href="/">Find out more</a
-					>
 				</div>
 			</div>
-			<div class="flex flex-col justify-between gap-5">
-				<div class="flex flex-col gap-3">
-					<img class="w-12" src="/responsible.png" alt="Responsible" />
-					<p class="font-display text-3xl font-semibold text-[#0C2D56]">Responsible</p>
-					<p class="font-body font-medium text-black">
-						Our products are biodegradable, produced responsibly and do not cause harm to people,
-						society or the environment.
+			<div
+				class="absolute right-0 top-[30%] z-20 aspect-square w-[75%] rounded-full bg-[#39375C] opacity-30"
+			></div>
+			<div
+				class="absolute bottom-0 left-[25%] z-30 aspect-square w-[40%] rounded-full bg-[#6B91D8] opacity-30"
+			></div>
+			<div
+				class="absolute -left-[25%] top-[30%] z-40 aspect-square w-[50%] rounded-full bg-[#5490BC] opacity-30"
+			></div>
+		</div>
+		<div class="col-span-2 row-span-2">
+			<div
+				class="relative z-40 ml-auto flex aspect-square w-[85%] items-center justify-center rounded-full bg-[#7FA1F8] text-[#0C2D56]"
+			>
+				<div class="w-2/3">
+					<p class="font-display mb-5 text-2xl font-semibold">Enriching<br />the planet</p>
+					<p class="font-body text-sm font-medium leading-loose">
+						We sell plant-based materials to enrich and promote the circular bioeconomy and
+						contribute to making the planet a healthier and safer place for generations to come.
 					</p>
-				</div>
-				<div>
-					<a
-						class="font-body self-start justify-self-end border-b border-black py-3 align-bottom font-bold"
-						href="/">Find out more</a
-					>
-				</div>
-			</div>
-			<div class="flex flex-col justify-between gap-5">
-				<div class="flex flex-col gap-3">
-					<img class="w-12" src="/climate-action.png" alt="Climate Action" />
-					<p class="font-display text-3xl font-semibold text-[#0C2D56]">Climate Action</p>
-					<p class="font-body font-medium text-black">
-						Our production avoids carbon emissions compared to existing material processing such as
-						titanium dioxide production.
-					</p>
-				</div>
-				<div>
-					<a class="font-body self-end border-b border-black py-3 font-bold" href="/"
-						>Find out more</a
-					>
 				</div>
 			</div>
 		</div>
