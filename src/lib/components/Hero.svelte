194f3266ffa147d4470288da08362e7238d0a95c
Liam Lyness <liamlyness97@gmail.com>
2024-05-30 20:26:22 +0100

    Hero section, and usp sections in place on desktop

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,31 @@
+<div
+	class="relative h-screen w-full rounded-lg bg-cover bg-no-repeat bg-blend-multiply"
+	style="background-image: url('/hero-bg.jpg'); background-color: rgba(72, 72, 83, 0.6);"
+>
+	<div class="flex w-full justify-between px-20 pt-14">
+		<div>
+			<img src="/seprify.png" alt="Seprify" />
+		</div>
+		<nav class="font-body flex gap-10 text-white">
+			<a href="/" title="About">About</a>
+			<a href="/" title="Technology">Technology</a>
+			<a href="/" title="Products">Products</a>
+			<a href="/" title="News">News</a>
+			<a href="/" title="Careers">Careers</a>
+		</nav>
+	</div>
+	<div
+		class="absolute left-0 right-0 top-0 mx-auto flex h-full max-w-7xl flex-col justify-center gap-5"
+	>
+		<p class="font-display w-[55%] text-pretty text-5xl font-medium leading-tight text-white">
+			Cellulose based high value added ingredients and formulations
+		</p>
+		<p class="font-body text-xl text-white">
+			Enabling sustainable products – customer driven – performance focused
+		</p>
+		<a
+			class="font-body self-start rounded-lg border border-white px-8 py-4 text-sm font-semibold text-white"
+			href="/">Find out more</a
+		>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<main class="p-5">
+	{@render children()}
+</main>
+
+<style></style>
